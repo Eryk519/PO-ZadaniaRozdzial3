7350475387e7e31318bcd7ad8a1b80f51c54d5c4
Eryk519 <74097448+Eryk519@users.noreply.github.com>
2021-11-12 19:12:16 +0100

    Add files via upload

---
+++ 3.4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Zad3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a,b,c,d,e,f;
+            string znak;
+            Console.WriteLine("Podaj pierwsza liczbe");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj druga liczbe");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Wybierz znak operacji ktora chcesz wykonac: +, -, *, /,  ");
+            znak = (Console.ReadLine());
+            c = a + b;
+            d = a - b;
+            e = a * b;
+            f = a / b;
+            if (znak == "+")
+                Console.WriteLine("Wynik dodawania wynosi: " + c);
+            if (znak == "-")
+                Console.WriteLine("Wynik dodawania wynosi: " + d);
+            if (znak == "*")
+                Console.WriteLine("Wynik dodawania wynosi: " + e);
+            if (znak == "/")
+                Console.WriteLine("Wynik dodawania wynosi: " + f);
+            Console.ReadKey();
+        }
+    }
+}
