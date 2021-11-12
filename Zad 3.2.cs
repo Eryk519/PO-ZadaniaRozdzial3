0940cdee7e30c582f461970bbc674eabe10a9c98
Eryk519 <74097448+Eryk519@users.noreply.github.com>
2021-11-12 19:11:47 +0100

    Add files via upload

---
+++ Zad 3.2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Zad_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Podaj pierwsza liczbe calkowita");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj druga liczbe calkowita");
+            b = int.Parse(Console.ReadLine());
+            if (a % b != 0)
+                {
+                    Console.WriteLine("{1} nie jest dzielnikiem {0}",a,b);
+                }
+            else
+                {
+                Console.WriteLine("{1} jest dzielnikiem {0}",a,b);
+                }
+            Console.ReadKey();
+        }
+    }
+}
