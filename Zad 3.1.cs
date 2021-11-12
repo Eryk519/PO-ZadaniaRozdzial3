a57cf5eb0971ce3b867db1f94a2c72e2ad636fe5
Eryk519 <74097448+Eryk519@users.noreply.github.com>
2021-11-12 19:11:37 +0100

    Add files via upload

---
+++ Zad 3.1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Zad_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rok;
+            Console.WriteLine("Podaj rok");
+            rok = int.Parse(Console.ReadLine());
+            if (rok % 4 == 0 && rok % 100 != 0 || rok % 400 == 0)
+                {
+                Console.WriteLine("Podany rok jest przestepny");
+                }
+            else
+                {
+                Console.WriteLine("Podany rok nie jest przestepny");
+                }
+            Console.ReadLine();
+
+        }
+    }
+}
